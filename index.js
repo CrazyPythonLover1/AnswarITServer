1c161c5448985288636ad164353d692717efe33d
md jahidul islam <za0162hid@gmail.com>
2021-06-04 11:48:37 +0600

    order send to the mongodb

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7adfu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = 5000;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const ordersCollection = client.db("answarIT").collection("personInfo");
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+});
+
+app.listen(port);
