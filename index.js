fc808875627620e3c4ce5aae2fd80a7260e66443
Md-Mainul <mainulislamfaruqi@gmail.com>
2021-06-19 13:21:41 +0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7adfu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgaci.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(express.json());
